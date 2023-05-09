cd1611fb2b2778e110563f33ee8e04d852462211
renatokov@gmail.com <renatokov@gmail.com>
2023-05-08 21:49:44 -0300

    first commit

---
+++ Projeto04/Interfaces/IEmpresaRepository.cs
@@ -0,0 +1,32 @@
+﻿using Projeto04.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto04.Interfaces
+{
+    //public interface IEmpresaRepository
+    //{
+    //    void Add(Empresa empresa);
+
+    //    void Update(Empresa empresa);
+
+    //    void Delete(Empresa empresa);
+
+    //    List<Empresa> GetAll();
+
+    //    Empresa? GetById(Guid idEmpresa);
+    //}
+
+    
+    namespace Projeto04.Interfaces
+    {
+        public interface IEmpresaRepository : IBaseRepository<Empresa>
+        {
+
+        }
+    }
+
+}
---
+++ Projeto04/Interfaces/IFuncionarioRepository.cs
@@ -0,0 +1,33 @@
+﻿using Projeto04.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto04.Interfaces
+{
+    //public interface IFuncionarioRepository
+    //{
+    //    void Add(Funcionario funcionario);
+
+    //    void Update(Funcionario funcionario);
+
+    //    void Delete(Funcionario funcionario);
+
+    //    List<Funcionario> GetAll();
+
+    //    Funcionario? GetById(Guid idFuncionario);
+    //}
+
+
+
+    namespace Projeto04.Interfaces
+    {
+        public interface IFuncionarioRepository : IBaseRepository<Funcionario>
+        {
+
+        }
+    }
+
+}
